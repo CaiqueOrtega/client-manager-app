28f1431d1ba03573eb4c52c3146e135fc7b79ed9
CaiqueOrtega <dev.caique.ortega@outlook.com>
2025-04-26 11:16:04 -0300

    refactor(firebase/services): remove declarações de console.log de debug dos serviços de auth e user
    - Remove declarações desnecessárias de console.log de debug do auth.service.ts
    - Remove console.log do user.service.ts

--- src/lib/firebase/service/auth.service.ts
+++ src/lib/firebase/service/auth.service.ts
@@ -4,27 +4,20 @@ import { auth, googleProvider } from '../client';
 
 export const AuthService = {
   async signInWithGoogle() {
-    console.log('🔑 Iniciando o login com Google...');
     const result = await signInWithPopup(auth, googleProvider);
-    console.log('🌀 Resultado do login com Google:', result);
 
     await new Promise((resolve) => {
-      console.log('⏳ Esperando o estado de autenticação...');
-
       const unsubscribe = auth.onAuthStateChanged((user) => {
         if (user) {
-          console.log('✅ Usuário autenticado:', user);
-          unsubscribe(); // Para de ouvir a mudança de autenticação
-          resolve(user); // Resolve com o usuário autenticado
+          unsubscribe();
+          resolve(user);
         } else {
           console.log('⚠️ Nenhum usuário autenticado');
         }
       });
     });
 
-    console.log('📝 Chamando UserService.createIfNotExists...');
     await UserService.createIfNotExists(result.user);
-    console.log('✅ Usuário verificado/criado no Firestore:', result.user);
 
     return result.user;
   },
--- src/lib/firebase/service/user.service.ts
+++ src/lib/firebase/service/user.service.ts
@@ -5,11 +5,9 @@ import { db } from '../client';
 export const UserService = {
   async createIfNotExists(user: User) {
     const userRef = doc(db, 'users', user.uid);
-    console.log('Verificando se o usuário existe no Firestore:', userRef.path);
 
     const snapshot = await getDoc(userRef);
     if (!snapshot.exists()) {
-      console.log('Usuário não existe. Criando novo usuário...');
       try {
         await setDoc(userRef, {
           uid: user.uid,
@@ -19,7 +17,6 @@ export const UserService = {
           createdAt: serverTimestamp(),
           lastLogin: serverTimestamp(),
         });
-        console.log('Usuário criado com sucesso!');
       } catch (error) {
         console.error('Erro ao criar documento no Firestore:', error);
       }
